8d0d144d6fb378ff022c1d6868b991cf80db1fd7
NjilaBoris <njilaboris2@gmail.com>
2025-09-10 01:20:00 +0100

    Footer V1

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import FAQs from "@/sections/FAQs";
+import Footer from "@/sections/Footer";
 import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
 import Intro from "@/sections/Intro";
@@ -15,6 +16,7 @@ const page = () => {
       <Projects />
       <Testimonials />
       <FAQs />
+      <Footer />
     </>
   );
 };
--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,5 +1,35 @@
+import Link from "next/link";
+
+const navItems = [
+  { href: "#", label: "Home" },
+  { href: "#", label: "Projects" },
+  { href: "#", label: "Testimonials" },
+  { href: "#", label: "Faqs" },
+  { href: "#", label: "Contact" },
+];
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-stone-900 text-white">
+      <div className="container">
+        <div className="py-24 md:py-32 lg:py-40">
+          <div>One spot available for next month</div>
+          <h2>Enough talk. Let&apos;s make something great together.</h2>
+          <button>tiakoloic@gmail.com</button>
+          <nav>
+            {navItems.map(({ href, label }) => (
+              <Link key={label} href={href}>
+                {label}
+              </Link>
+            ))}
+          </nav>
+        </div>
+        <p className="py-16">
+          Copyright &copy; Tiako Loic &bull; All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
